6cc94351e1f144c04f653dd11e967578f3ebb73d
Hoggspjot <vingisaar@gmail.com>
2025-08-27 23:46:40 +0300

    initial commit

---
+++ HomeWork/src/main/java/com/MySpringHomeWork/HomeWork/MySpringHomeWorkApplication.java
@@ -0,0 +1,13 @@
+package com.MySpringHomeWork.HomeWork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringHomeWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringHomeWorkApplication.class, args);
+	}
+
+}
